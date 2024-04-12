91c4e327432d4ba4c1272eabd97f72dfd149c531
shadow3aaa <1394025007@qq.com>
2024-04-12 21:59:55 +0800

    App卡片使用圆形图标

--- app/src/main/java/com/fasRs/manager/PackageInfo.kt
+++ app/src/main/java/com/fasRs/manager/PackageInfo.kt
@@ -1,8 +1,5 @@
 package com.fasRs.manager
 
 import android.graphics.Bitmap
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
-data class PackageInfo(val appName: String, val pkgName: String, val icon: Bitmap) : Parcelable
+data class PackageInfo(val appName: String, val pkgName: String, val icon: Bitmap)
--- app/src/main/java/com/fasRs/manager/modeSetting/AppCard.kt
+++ app/src/main/java/com/fasRs/manager/modeSetting/AppCard.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -14,10 +15,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -44,7 +44,7 @@ fun AppCard(
                 .padding(25.dp),
         elevation =
             CardDefaults.cardElevation(
-                defaultElevation = 5.dp,
+                defaultElevation = 3.dp,
             ),
         shape = RoundedCornerShape(20.dp),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
@@ -80,7 +80,8 @@ fun AppCard(
                 modifier =
                     Modifier
                         .padding(25.dp)
-                        .size(75.dp),
+                        .size(75.dp)
+                        .clip(CircleShape),
                 bitmap = icon!!,
                 contentDescription = null,
                 tint = Color.Unspecified,
