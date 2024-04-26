dc76eb4985c171a9cffb3e7a3e0eb38bea5e4fd9
shadow3aaa <1394025007@qq.com>
2024-04-26 21:13:45 +0800

    update

--- app/src/main/java/com/fasRs/manager/Collections.kt
+++ app/src/main/java/com/fasRs/manager/Collections.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -121,6 +120,7 @@ fun FilterSticker(
     label: String = "preview",
 ) {
     FilterChip(
+        modifier = modifier,
         selected = selected,
         onClick = {
             onClick()
