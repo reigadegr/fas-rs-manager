77500a79b8aac7eee75595600fd7875450261212
shadow3aaa <1394025007@qq.com>
2024-04-27 13:31:47 +0800

    update

--- app/src/main/java/com/fasRs/manager/PackageInfo.kt
+++ app/src/main/java/com/fasRs/manager/PackageInfo.kt
@@ -1,28 +1,30 @@
 package com.fasRs.manager
 
-import android.content.Context
 import android.graphics.Bitmap
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.core.content.res.ResourcesCompat
 
 data class PackageInfo(val appName: String, val pkgName: String, val icon: Bitmap, val system: Boolean)
 
 @Composable
-fun thisPackageInfo(context: Context): PackageInfo {
+fun thisPackageInfo(): PackageInfo {
+    val context = LocalContext.current
+
     return PackageInfo(
         appName = stringResource(id = R.string.app_name),
         pkgName = stringResource(id = R.string.app_name),
         icon =
-        ResourcesCompat.getDrawable(
-            context.resources,
-            R.mipmap.ic_launcher_round,
-            context.theme,
-        )!!.let { drawable ->
-            appIconToBitmap(drawable)
-        }.asImageBitmap().asAndroidBitmap(),
+            ResourcesCompat.getDrawable(
+                context.resources,
+                R.mipmap.ic_launcher_round,
+                context.theme,
+            )!!.let { drawable ->
+                appIconToBitmap(drawable)
+            }.asImageBitmap().asAndroidBitmap(),
         system = false,
     )
 }
